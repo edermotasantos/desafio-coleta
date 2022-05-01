cae18073ecfcf31b459d1d3e386bf4f1e242b2b3
Eder Santos <3derms@gmail.com>
2022-05-01 12:01:05 -0300

    fix: corrige página da rota para create

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 const Answers = require('../controllers/answers');
 
 routes.get('/', Answers.index);
-routes.post('/answers', Answers.create);
+routes.post('/', Answers.create);
 routes.get('/answers', Answers.readAllAnswers);
 
 module.exports = routes;
