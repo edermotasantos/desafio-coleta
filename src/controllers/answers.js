24c9068ef57714e94e9996530af3f96ed31adaad
Eder Santos <3derms@gmail.com>
2022-05-01 10:31:13 -0300

    fix: corrige função para contar quantidade de respostas

--- src/controllers/answers.js
+++ src/controllers/answers.js
@@ -1,38 +1,45 @@
 const Answers = require('../services/answers');
-const ModelAnswers = require('../models/answers')
+const ModelAnswers = require('../models/answers');
 const fs = require('fs');
 
-let last_id = undefined;
-let positiveQuantity = 0;
-let negativeQuantity = 0;
-let unassessedQuantity = 0;
 const arrayOfJsonAnswers = [];
+const obj = {};
 
 const index = (_req, res) => {
   res.json({ message: 'Respostas regitradas com sucesso com sucesso!' });
 };
 
-const findLastAnswers = async (last_id, Pergunta1, Pergunta2, Pergunta3) => {
-  if (Pergunta1 === 'Sim' || Pergunta2 === 'Sim' || Pergunta3 === 'Sim' ) return positiveQuantity = 1;
-  if (Pergunta1 === 'Não' || Pergunta2 === 'Não' || Pergunta3 === 'Não') return negativeQuantity = 1;
-  if (Pergunta3 === 'Agora!!') return unassessedQuantity = 1;
-  if (typeof last_id === 'undefined') {
-    return {
-      'QuantidadePositiva': 0 + positiveQuantity,
-      'QuantidadeNegativa': 0 + negativeQuantity,
-      'QuantidadeNaoAvaliada': 0 + unassessedQuantity,
-    };
+const answerCounter = (Pergunta1, Pergunta2, Pergunta3) => {
+  if (Object.keys(obj).length === 0) {
+    obj['S'] = 0; // 'S' caso a resposta for “Sim” ou “Agora
+    obj['N'] = 0; // 'N' caso a resposta for “Não
+    obj['A'] = 0; // 'A' caso a resposta for “Não Sei”
+    obj[Pergunta1[0]] += 1;
+    obj[Pergunta2[0]] += 1;
+    if (Pergunta3 === 'Não Sei'){
+      obj['A'] += 1;
+      return obj;
+    }
+    if (Pergunta3 === 'Agora!!'){
+      obj['S'] += 1;
+      return obj;
+    }
+    obj[Pergunta3[0]] += 1;
+    return obj;
   }
-  const {
-    QuantidadePositiva,
-    QuantidadeNegativa,
-    QuantidadeNaoAvaliada
-  } = await Answers.findOne({ last_id });
-  return {
-    'QuantidadePositiva': QuantidadePositiva + positiveQuantity,
-    'QuantidadeNegativa': QuantidadeNegativa + negativeQuantity,
-    'QuantidadeNaoAvaliada': QuantidadeNaoAvaliada + unassessedQuantity,
-  };
+
+  obj[Pergunta1[0]] += 1;
+  obj[Pergunta2[0]] += 1;
+  if (Pergunta3 === 'Não Sei'){
+    obj['A'] += 1;
+    return obj;
+  }
+  if (Pergunta3 === 'Agora!!'){
+    obj['S'] += 1;
+    return obj;
+  }
+  obj[Pergunta3[0]] += 1;
+  return obj;
 };
 
 const writeFile = async (_id, Pergunta1, Pergunta2, Pergunta3, Pergunta4, QuantidadePositiva, QuantidadeNegativa, QuantidadeNaoAvaliada) => {
@@ -40,7 +47,7 @@ const writeFile = async (_id, Pergunta1, Pergunta2, Pergunta3, Pergunta4, Quanti
   const answersString = JSON.stringify(arrayOfJsonAnswers);
   fs.writeFile('coleta-de-respostas.txt', answersString, (err) => {
     if (err) throw err;
-    console.log('O criado com sucesso!');
+    console.log('Arquivo coleta-de-respostas.txt criado com sucesso!');
   });
 };
 
@@ -48,35 +55,21 @@ const create = async (req, res) => {
   try {
     const { Pergunta1, Pergunta2, Pergunta3, Pergunta4 } = req.body;
     let data = {};
-    const { 
-      QuantidadePositiva,
-      QuantidadeNegativa,
-      QuantidadeNaoAvaliada,
-    } = await findLastAnswers(last_id, Pergunta1, Pergunta2, Pergunta3);
-    
+    const { S, N, A } = answerCounter(Pergunta1, Pergunta2, Pergunta3);
     data = {
       Pergunta1,
       Pergunta2,
       Pergunta3,
       Pergunta4,
-      QuantidadePositiva,
-      QuantidadeNegativa,
-      QuantidadeNaoAvaliada,
+      QuantidadePositiva: S,
+      QuantidadeNegativa: N,
+      QuantidadeNaoAvaliada: A,
     };
   
-    const { _id} = await Answers.create(data);
-    last_id = _id;
+    const { _id } = await Answers.create(data);
+    const { QuantidadePositiva, QuantidadeNegativa, QuantidadeNaoAvaliada } = data; 
     writeFile(_id, Pergunta1, Pergunta2, Pergunta3, Pergunta4, QuantidadePositiva, QuantidadeNegativa, QuantidadeNaoAvaliada);
-    return res.status(201).json({ 
-      _id,
-      Pergunta1,
-      Pergunta2,
-      Pergunta3,
-      Pergunta4,
-      QuantidadePositiva,
-      QuantidadeNegativa,
-      QuantidadeNaoAvaliada,
-    });
+    return res.status(201).json({ _id, Pergunta1, Pergunta2, Pergunta3, Pergunta4, QuantidadePositiva, QuantidadeNegativa, QuantidadeNaoAvaliada });
   } catch (error) {
     return res.status(500).json({ mensage: 'please, try later' });
   }
