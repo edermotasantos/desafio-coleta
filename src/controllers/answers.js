c8385d6e4d8e6328fccd4f5303cd0a557c0c5c95
Eder Santos <3derms@gmail.com>
2022-05-01 10:50:29 -0300

    refactor: refatora answerCounter

--- src/controllers/answers.js
+++ src/controllers/answers.js
@@ -14,18 +14,6 @@ const answerCounter = (Pergunta1, Pergunta2, Pergunta3) => {
     obj['S'] = 0; // 'S' caso a resposta for “Sim” ou “Agora
     obj['N'] = 0; // 'N' caso a resposta for “Não
     obj['A'] = 0; // 'A' caso a resposta for “Não Sei”
-    obj[Pergunta1[0]] += 1;
-    obj[Pergunta2[0]] += 1;
-    if (Pergunta3 === 'Não Sei'){
-      obj['A'] += 1;
-      return obj;
-    }
-    if (Pergunta3 === 'Agora!!'){
-      obj['S'] += 1;
-      return obj;
-    }
-    obj[Pergunta3[0]] += 1;
-    return obj;
   }
 
   obj[Pergunta1[0]] += 1;
