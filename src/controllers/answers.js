5f8bf489f6f51d3cf51caa6d5226476d1f113398
Eder Santos <3derms@gmail.com>
2022-05-01 11:21:13 -0300

    refactor: cria arquivo para armazenar os contadores

--- src/controllers/answers.js
+++ src/controllers/answers.js
@@ -1,33 +1,12 @@
 const Answers = require('../services/answers');
+const Counters = require('../util/counters');
 const ModelAnswers = require('../models/answers');
 const fs = require('fs');
 
 const arrayOfJsonAnswers = [];
-const obj = {};
 
 const index = (_req, res) => {
   res.json({ message: 'Respostas regitradas com sucesso com sucesso!' });
-};
-
-const answerCounter = (Pergunta1, Pergunta2, Pergunta3) => {
-  if (Object.keys(obj).length === 0) {
-    obj['S'] = 0; // 'S' caso a resposta for “Sim” ou “Agora
-    obj['N'] = 0; // 'N' caso a resposta for “Não
-    obj['A'] = 0; // 'A' caso a resposta for “Não Sei”
-  }
-
-  obj[Pergunta1[0]] += 1; // A primeira letra é o mesmo nome da chave que será somada
-  obj[Pergunta2[0]] += 1; // pode ser "Não"[0] = "N" ou "Sim"[0] = "S" 
-  if (Pergunta3 === 'Não Sei'){
-    obj['A'] += 1;
-    return obj;
-  }
-  if (Pergunta3 === 'Agora!!'){
-    obj['S'] += 1;
-    return obj;
-  }
-  obj[Pergunta3[0]] += 1;
-  return obj;
 };
 
 const writeFile = async (data) => {
@@ -43,7 +22,7 @@ const create = async (req, res) => {
   try {
     const { Pergunta1, Pergunta2, Pergunta3, Pergunta4 } = req.body;
     let data = {};
-    const { S, N, A } = answerCounter(Pergunta1, Pergunta2, Pergunta3);
+    const { S, N, A } = Counters.answerCounter(Pergunta1, Pergunta2, Pergunta3);
 
     // 'S' caso a resposta for “Sim” ou “Agora
     // 'N' caso a resposta for “Não
